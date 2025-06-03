db1e307f257af0d4620c51a51269a7f78045254b
erfan-ne <erfan.neshati@gmail.com>
2025-06-03 22:33:25 +0330

    feat: add remove option for courses page

--- dashboard/courses/script.js
+++ dashboard/courses/script.js
@@ -2,54 +2,114 @@ const data = {
     courses: [],
   };
   
-  const toggleMenu = document.querySelector(".toggle-sidebar");
-  const coursesBody = document.querySelector(".table-body");
-  const coursesData = document.querySelectorAll(".courses-data");
-  const pagination = document.querySelector(".pagination");
-  
-  let page = 1;
-  let coursePerPage = 6;
-  
-  const coursesSection = () => {
-    fetch("https://js-cms.iran.liara.run/api/courses")
-      .then((response) => response.json())
-      .then((courses) => {
-        data.courses.push(...courses);
-  
-        renderCourses(page);
-        renderPagination();
-  
-        coursesData.forEach((course) => (course.innerHTML = data.courses.length));
-      });
-  };
-  
-  function renderCourses(currentPage) {
-    coursesBody.innerHTML = "";
-    const startIndex = (currentPage - 1) * coursePerPage;
-    const endIndex = startIndex + coursePerPage;
-    const showCourses = data.courses.slice(startIndex, endIndex);
-  
-    showCourses.forEach(function (course) {
-      coursesBody.insertAdjacentHTML(
-        "beforeend",
-        `
-        <div class="tableRow">
-          <p class="course-title">${course.title}</p>
-          <p class="course-price">${course.price.toLocaleString()}</p>
-          <p class="course-category">${course.category}</p>
-          <div class="course-manage">
-            <button class="edit-btn" onclick="showEditCourseModal(${course.id})">
-              <i class="fas fa-edit"></i>
-            </button>
-            <button class="remove-btn" onclick="showRemoveCourseModal(${course.id})">
-              <i class="fas fa-trash-alt"></i>
-            </button>
-          </div>
-        </div>
-        `
-      );
+const toggleMenu = document.querySelector(".toggle-sidebar");
+const coursesBody = document.querySelector(".table-body");
+const coursesData = document.querySelectorAll(".courses-data");
+const pagination = document.querySelector(".pagination");
+const modalScreen = document.querySelector(".modal-screen");
+const toast = document.querySelector(".toast");
+
+let page = 1;
+let coursePerPage = 6;
+
+const coursesSection = () => {
+  fetch("https://js-cms.iran.liara.run/api/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+      data.courses.push(...courses);
+
+      renderCourses(page);
+      renderPagination();
+
+      coursesData.forEach((course) => (course.innerHTML = data.courses.length));
     });
-  }
+};
+
+function renderCourses(currentPage) {
+  coursesBody.innerHTML = "";
+  const startIndex = (currentPage - 1) * coursePerPage;
+  const endIndex = startIndex + coursePerPage;
+  const showCourses = data.courses.slice(startIndex, endIndex);
+
+  showCourses.forEach(function (course) {
+    coursesBody.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="tableRow">
+        <p class="course-title">${course.title}</p>
+        <p class="course-price">${course.price.toLocaleString()}</p>
+        <p class="course-category">${course.category}</p>
+        <div class="course-manage">
+          <button class="edit-btn" onclick="showEditCourseModal(${course.id})">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button class="remove-btn" onclick="showRemoveCourseModal('${course._id}')">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
+      </div>
+      `
+    );
+  });
+}
+
+const showRemoveCourseModal = (courseID) => {
+  modalScreen.classList.remove("hidden");
+  modalScreen.innerHTML = "";
+  modalScreen.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="modal">
+      <i class="ui-border top red"></i>
+      <i class="ui-border bottom red"></i>
+      <header class="modal-header">
+        <h3>حذف دوره</h3>
+        <button class="close-modal" onclick="hideRemoveCourseModal()">
+          <i class="fas fa-times"></i>
+        </button>
+      </header>
+      <main class="modal-content">
+        <p class="remove-text">آیا از حذف این دوره اطمینان دارید؟</p>
+      </main>
+      <footer class="modal-footer">
+        <button class="cancel" onclick="hideRemoveCourseModal()">انصراف</button>
+        <button class="submit" onclick="removeCourse('${courseID}')">تائید</button>
+      </footer>
+    </div>
+    `
+  );
+};
+
+const hideRemoveCourseModal = () => modalScreen.classList.add("hidden")
+
+const removeCourse = (courseID) => {
+  fetch(`https://js-cms.iran.liara.run/api/courses/${courseID}`, { method: "DELETE" })
+      .then((response) => {
+          if (response.status === 200) {
+            const process = toast.querySelector(".process");
+            const toastText = toast.querySelector(".toast-content");
+            toast.classList.remove("hidden");
+            toastText.innerHTML = "دوره با موفقیت حذف شد";
+          
+            let timer;
+            let processWidth = 0;
+          
+            timer = setInterval(function () {
+              process.style.width = `${processWidth++}%`;
+              if (processWidth === 120) {
+                clearInterval(timer);
+                toast.classList.add("hidden");
+                process.style.width = "0%";
+                processWidth = 0;
+              }
+            }, 50);
+        
+            coursesSection();
+            hideRemoveCourseModal();
+          }
+      });
+};
+
   
   function renderPagination() {
     const pagesCount = Math.ceil(data.courses.length / coursePerPage);
