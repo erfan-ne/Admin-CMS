82f4345cdab58bbb69c8064bbc553e2a08f18095
erfan-ne <erfan.neshati@gmail.com>
2025-06-04 13:18:08 +0330

    feat: add course modal in courses page

--- dashboard/courses/script.js
+++ dashboard/courses/script.js
@@ -8,6 +8,7 @@ const coursesData = document.querySelectorAll(".courses-data");
 const pagination = document.querySelector(".pagination");
 const modalScreen = document.querySelector(".modal-screen");
 const toast = document.querySelector(".toast");
+const createCourseBtn = document.querySelector("#create-course")
 
 let page = 1;
 let coursePerPage = 6;
@@ -52,19 +53,42 @@ function renderCourses(currentPage) {
     );
   });
 }
+  
+function renderPagination() {
+  const pagesCount = Math.ceil(data.courses.length / coursePerPage);
+  if (pagination) {
+    pagination.innerHTML = "";
+    for (let i = 0; i < pagesCount; i++) {
+      pagination.insertAdjacentHTML(
+        "beforeend",
+        `<span class="page ${i === 0 ? "active" : ""}" onclick="changePageHandler(${i + 1})">${i + 1}</span>`
+      );
+    }
+  }
+}
+
+window.changePageHandler = function (userSelectedPage) {
+  page = userSelectedPage;
+
+  const pageNumbers = document.querySelectorAll(".page");
+  pageNumbers.forEach((number) => {
+    number.classList.toggle("active", +number.innerHTML === page);
+  });
+
+  renderCourses(page);
+};
 
 const showRemoveCourseModal = (courseID) => {
   modalScreen.classList.remove("hidden");
   modalScreen.innerHTML = "";
-  modalScreen.insertAdjacentHTML(
-    "beforeend",
+  modalScreen.insertAdjacentHTML("beforeend",
     `
     <div class="modal">
       <i class="ui-border top red"></i>
       <i class="ui-border bottom red"></i>
       <header class="modal-header">
         <h3>حذف دوره</h3>
-        <button class="close-modal" onclick="hideRemoveCourseModal()">
+        <button class="close-modal" onclick="hideModal()">
           <i class="fas fa-times"></i>
         </button>
       </header>
@@ -72,7 +96,7 @@ const showRemoveCourseModal = (courseID) => {
         <p class="remove-text">آیا از حذف این دوره اطمینان دارید؟</p>
       </main>
       <footer class="modal-footer">
-        <button class="cancel" onclick="hideRemoveCourseModal()">انصراف</button>
+        <button class="cancel" onclick="hideModal()">انصراف</button>
         <button class="submit" onclick="removeCourse('${courseID}')">تائید</button>
       </footer>
     </div>
@@ -80,7 +104,7 @@ const showRemoveCourseModal = (courseID) => {
   );
 };
 
-const hideRemoveCourseModal = () => modalScreen.classList.add("hidden")
+const hideModal = () => modalScreen.classList.add("hidden")
 
 const removeCourse = (courseID) => {
   fetch(`https://js-cms.iran.liara.run/api/courses/${courseID}`, { method: "DELETE" })
@@ -105,39 +129,60 @@ const removeCourse = (courseID) => {
             }, 50);
         
             coursesSection();
-            hideRemoveCourseModal();
+            hideModal();
           }
       });
 };
 
-  
-  function renderPagination() {
-    const pagesCount = Math.ceil(data.courses.length / coursePerPage);
-    if (pagination) {
-      pagination.innerHTML = "";
-      for (let i = 0; i < pagesCount; i++) {
-        pagination.insertAdjacentHTML(
-          "beforeend",
-          `<span class="page ${i === 0 ? "active" : ""}" onclick="changePageHandler(${i + 1})">${i + 1}</span>`
-        );
-      }
-    }
-  }
-  
-  window.changePageHandler = function (userSelectedPage) {
-    page = userSelectedPage;
-  
-    const pageNumbers = document.querySelectorAll(".page");
-    pageNumbers.forEach((number) => {
-      number.classList.toggle("active", +number.innerHTML === page);
-    });
-  
-    renderCourses(page);
-  };
-  
-  toggleMenu.addEventListener("click", function () {
-    document.querySelector(".sidebar").classList.toggle("open");
-  });
-  
-  coursesSection();
+const showCreateCourseModal = () => {
+  modalScreen.classList.remove("hidden")
+  modalScreen.innerHTML = "";
+  modalScreen.insertAdjacentHTML("beforeend",
+    `
+    <div class="modal">
+      <i class="ui-border top red"></i>
+      <i class="ui-border bottom red"></i>
+        <header class="modal-header">
+          <h3>ایجاد دوره</h3>
+          <button class="close-modal" onclick="hideModal()">
+            <i class="fas fa-times"></i>
+          </button>
+        </header>
+        <main class="modal-content">
+          <input
+            type="text"
+            class="modal-input"
+            placeholder="عنوان دوره را وارد نمائید ..."
+            id="course-title"
+          />
+          <input
+            type="number"
+            class="modal-input"
+            placeholder="قیمت دوره را وارد نمائید ..."
+            id="course-price"
+          />
+          <input
+            type="number"
+            class="modal-input"
+            placeholder="دسته بندی دوره را وارد نمائید ..."
+            id="course-category"
+          />
+        </main>
+        <footer class="modal-footer">
+          <button class="cancel" onclick="hideModal()">انصراف</button>
+          <button class="submit" onclick="createNewCourse()">تائید</button>
+        </footer>
+    </div>
+    `
+  );
+}
+
+
+
+createCourseBtn.addEventListener("click" , showCreateCourseModal)
+toggleMenu.addEventListener("click", function () {
+  document.querySelector(".sidebar").classList.toggle("open");
+});
+
+coursesSection();
   
